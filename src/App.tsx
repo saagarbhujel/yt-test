8d21f820efcbe3c24515367aca121763009bb81a
saagarbhujel <saagarbhujel22@gmail.com>
2023-10-11 20:55:41 +0545

    routes created

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
+import Layout from "./Layout";
+import AdminLogin from "./components/admin/adminLogin";
+import Dashboard from "./components/admin/components/dashboard";
 import Login from "./components/login"
-// import Signup from "./components/signup"
+import Signup from "./components/signup"
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-   <main>
-    {/* <Signup/> */}
-    <Login/>
-   </main>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+          {/* Public Routes */}
+          <Route path='login' element={<Login />} />
+          <Route path='signup' element={<Signup />} />
+          <Route path='admim/login' element={<AdminLogin />} />
+
+          {/* Protected Routes */}
+         <Route>
+         <Route path="admin/dashboard" element={<Dashboard/>} />
+         </Route>
+
+
+
+
+          {/* Catch for all */}
+          <Route path='*' element={<h1>404 Not Found</h1>} />
+      </Route>
+    </Routes>
   )
 }
 
