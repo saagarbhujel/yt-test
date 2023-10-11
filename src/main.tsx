5ea33cf2466da91f1a6317efae90c5bb64705177
saagarbhujel <saagarbhujel22@gmail.com>
2023-10-11 20:55:28 +0545

    wrapped with router

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext.tsx'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <BrowserRouter>
     <AuthProvider>
-    <App />
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
     </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
