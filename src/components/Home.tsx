d5238d850315425e2e0ccb1586ef62c714fe33fd
saagarbhujel <saagarbhujel22@gmail.com>
2023-10-13 02:54:00 +0545

    some lonk added

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+
+import {Link} from "react-router-dom";
+import Player from './Player'
+import useLogout from "../hooks/useLogout.ts";
 
 const Home = () => {
+    const logout = useLogout()
+    const handleClick = () => {
+    logout()
+    }
   return (
-    <div>Home</div>
+    <div>Home
+     <Player />
+      <Link to={"/profile"}>Profile</Link>
+      <Link to={"/dashboard"}>Dashboard</Link>
+       < button onClick={handleClick}>Logout</button>
+    </div>
   )
 }
 
