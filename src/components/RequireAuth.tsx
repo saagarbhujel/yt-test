86ae9235c52a6f2277f6548e690d16e6f196ca4d
saagarbhujel <saagarbhujel22@gmail.com>
2023-10-13 02:56:25 +0545

    require auth refactored

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,25 +1,35 @@
 import React from 'react'
 import { useLocation,Navigate, Outlet } from 'react-router-dom'
 import useAuth from '../hooks/useAuth'
+import jwt_decode from "jwt-decode";
 
 
 
 type RequireAuthProps = {
   allowedRoles: string[];
 };
+type AuthToken = {
+  role: string;
 
+};
 
 const RequireAuth: React.FC<RequireAuthProps> = ({ allowedRoles }) => {
-  const { auth } = useAuth() 
+  const { auth } = useAuth()
   const location = useLocation();
+  const decoded:AuthToken | undefined = auth.accessToken ? jwt_decode(auth.accessToken) : undefined
+  // console.log(decoded?.role)
+  const role = decoded?.role ? decoded?.role : undefined
+  // console.log(role)
 
   return (
-  auth?.role && allowedRoles?.includes(auth?.role) ? 
+      role && allowedRoles?.includes(role) ?
     <Outlet />
-   : auth?.user
+   : !auth?.user
                 ? <Navigate to="/unauthorized" state={{ from: location }} replace />
                 : <Navigate to="/login" state={{ from: location }} replace />
   )
 };
+
+
 
 export default RequireAuth
