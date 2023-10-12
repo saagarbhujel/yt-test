877c11e4f74e775bba30c074a89c67ec6826e80d
saagarbhujel <saagarbhujel22@gmail.com>
2023-10-13 02:53:42 +0545

    axios private to invoke intercptor

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,14 @@
 import axios from 'axios';
+const BASE_URL = 'http://localhost:3001';
 
 export default axios.create({
-  baseURL: "http://192.168.18.127:3001",
+  baseURL: BASE_URL,
 });
+
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+})
