4a1dec39a3e1c42cca87dc9f5758ebf3552b13ac
saagarbhujel <saagarbhujel22@gmail.com>
2023-10-14 20:04:51 +0545

    left aside complete

--- src/components/LeftAside.tsx
+++ src/components/LeftAside.tsx
@@ -1,26 +1,62 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+
+import { Link } from "react-router-dom";
+import { useActiveSectionContext } from "../context/active-section-context";
+import { navLinks } from "./constants/Credentials";
+import clsx from "clsx";
 
 const LeftAside = () => {
+
+     const { active, setActive, setTimeOfLastClick } = useActiveSectionContext();
+
   return (
-  <section className='w-[18vw] h-[100vh] flex flex-col bg-[#173244] border-r  border-black'>
-    <h2 className=' text-center my-3 text-[34px] font-bold text-white'>Yarsa Play</h2>
-    <div className=' '>
-     <div className='pl-6  text-white'>
-        <h5 className='text-[18px] font-bold'>Dashboard</h5>
-        <ul className=''>
-            <Link to='/'> <li>Homepage</li></Link>
-           <Link to='/profile'> <li>Profile</li></Link>
-           <Link to='/chat'> <li>Chats</li></Link>
-           <Link to='/stat'> <li>Stats</li></Link>
-           
-        </ul>
-     </div>
+    <>
+      <section className="  w-[20vw] h-[100vh] flex items-center  flex-col border-r   ">
+        <div className="">
+          <div className="mt-8 mb-12">
+            <h1 className="text-center  text-[32px] font-bold">
+              {/* <Link to={"/"}>Yarsha Play</Link> */}
+            </h1>
+          </div>
 
-    </div>
+          <div className="     w-[17vw] h-[90vh] rounded-md mt-[8rem]">
+            <ul className="flex  flex-col justify-center md:items-center items-start ml-[4px] ">
+              {navLinks.map((link) => (
+                <Link
+                  className={clsx("w-full  text-gray-400  transition btn", {
+                    "text-black": active === link.title,
+                  })}
+                  to={link.path}
+                  onClick={() => {
+                    {
+                      setActive(link.title);
+                      setTimeOfLastClick(Date.now());
+                    }
+                  }}
+                >
+                  <li
+                    key={link.id}
+                    className={clsx(
+                      " w-[30px] md:w-[204px] cursor-pointer hover:bg-blue-600/95 hover:text-black  md:pr-[24px] md:pt-[16px] md:pb-[16px] rounded-[16px] mb-[5px] mt-[5px] flex items-center  ",
+                      {
+                        " bg-blue-600/95 text-white hover:text-white h-f[20px] w-[30px] ":
+                          active === link.title,
+                      }
+                    )}
+                  >
+                    <p className="hidden md:block w-full   pl-[24px]">
+                      {link.title}
+                    </p>
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-  </section>
-  )
-}
+
 
 export default LeftAside
