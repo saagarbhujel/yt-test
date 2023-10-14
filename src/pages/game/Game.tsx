73c45a1226941f9a1d3d0d8e79f0d5110d052ab6
saagarbhujel <saagarbhujel22@gmail.com>
2023-10-14 20:08:45 +0545

    game component

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
-
+import LeftAside from "../../components/LeftAside";
+import NavBar from "../home/NavBar";
+import PlayGame from './PlayGame';
 
 const Game = () => {
   return (
-   
-   
-    <div>Game</div>
-  
-  )
+    <section className="flex">
+      <div className="fixed">
+        <LeftAside />
+
+      </div>
+      <div className=" ml-[20vw]">
+        <NavBar />
+        <PlayGame />
+      </div>
+    </section>
+  );
 }
 
 export default Game
