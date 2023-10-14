581faa2f07a1a63ce4d0e5319e6c84f301ee3f94
saagarbhujel <saagarbhujel22@gmail.com>
2023-10-14 20:04:37 +0545

    wrapped with active section context

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,18 @@ import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext.tsx'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import ActiveSectionContext from '../src/context/active-section-context.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-    <AuthProvider>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </AuthProvider>
+      <AuthProvider>
+        <ActiveSectionContext>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </ActiveSectionContext>
+      </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
