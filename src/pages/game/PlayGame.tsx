dcf3c13186822b5585321611d25d87a3e716e5eb
saagarbhujel <saagarbhujel22@gmail.com>
2023-10-15 09:55:53 +0545

    play game message added

--- src/pages/game/PlayGame.tsx
+++ src/pages/game/PlayGame.tsx
@@ -18,22 +18,30 @@ const PlayGame = () => {
     const [message, setMessage] = React.useState('')
 
   const playGame = async () => {
-       const res = await axiosPrivate.get('/player/play/game',  {
-              signal: controller.signal,
-            }
-        )
-        // console.log(res);
-        // console.log(res?.data);
-        // console.log(res?.data.data.games_played);
-        // console.log(res?.data.message);
+    try {
+      const res = await axiosPrivate.get('/player/play/game',  {
+        signal: controller.signal,
+      }
+  )
+  // console.log(res);
+  // console.log(res?.data);
+  // console.log(res?.data.data.games_played);
+  // console.log(res?.data.message);
 
-        setCoins(res?.data.data.coins)
-        setExperiencePoint(res?.data.data.experience_point)
-        setGamePlayed(res?.data.data.games_played)
-        setGameWon(res?.data.data.games_won)
-        setMessage(res?.data.message)
-       
-         isMounted 
+  setCoins(res?.data.data.coins)
+  setExperiencePoint(res?.data.data.experience_point)
+  setGamePlayed(res?.data.data.games_played)
+  setGameWon(res?.data.data.games_won)
+  setMessage(res?.data.message)
+ 
+   isMounted 
+    } catch (error) {
+      console.log(error,"Error Message");
+      
+    }finally{
+      setMessage('')
+    }
+      
     }
 
     useEffect(()=>{
@@ -46,6 +54,28 @@ const PlayGame = () => {
         }
     },[])
 
+    const messageHandler = () => {
+
+        if(message === "game lost"){
+          return  <p className="text-red-500 text-[24px]">
+            Sorry! you lost the game.
+          </p>
+          } else if(message === "game won"){
+            return   <p className="text-blue-600 text-[24px]">
+            Congrulation you won the game.
+          </p>
+          } else{
+            return   <p className="text-blue-600 text-[24px]">
+            Play game.</p>
+          }
+
+
+      
+    
+    }
+
+    const messageDeliver = messageHandler()
+   
     
 
     
@@ -53,7 +83,7 @@ const PlayGame = () => {
     <div className="h-[80vh]  mt-8">
       <div className="h-[70%] flex flex-col  ">
         <section className="flex justify-evenly h-[70%]">
-          <div className="flex flex-col justify-evenly h-36 items-center mt-16 mb-16  bg-slate-400/20 w-[15rem] shadow-md ">
+          <div className="flex flex-col justify-evenly h-36 items-center mt-16 mb-16  bg-green-400/10 w-[15rem] shadow-md ">
             <span className="text-[4rem] text-blue-600 pl-6 pr-6">
               {gamePlayed}
             </span>
@@ -63,7 +93,7 @@ const PlayGame = () => {
             </p>
           </div>
 
-          <div className="flex flex-col justify-evenly h-36 items-center mt-16 mb-16 bg-slate-400/20 w-[15rem] shadow-md">
+          <div className="flex flex-col justify-evenly h-36 items-center mt-16 mb-16 bg-green-400/10 w-[15rem] shadow-md">
             <span className="text-[4rem] text-blue-600 pl-6 pr-6">
               {gameWon}
             </span>
@@ -72,36 +102,28 @@ const PlayGame = () => {
             </p>
           </div>
 
-          <div className="flex flex-col justify-evenly h-36 items-center mt-16 mb-16 bg-slate-400/20 w-[15rem] shadow-md">
+          <div className="flex flex-col justify-evenly h-36 items-center mt-16 mb-16 bg-green-400/10 w-[15rem] shadow-md">
             <span className="text-[4rem] text-blue-600 pl-6 pr-6">
               {experiencePoint}
             </span>
             <p className="text-xl font-semibold text-blue-600 pb-4">Points</p>
           </div>
 
-          <div className="flex flex-col justify-evenly h-36 items-center mt-16 mb-16 bg-slate-400/20 w-[15rem] shadow-md">
+          <div className="flex flex-col justify-evenly h-36 items-center mt-16 mb-16 bg-green-400/10 w-[15rem] shadow-md">
             <span className="text-[4rem] text-blue-600 pl-6 pr-6">{coins}</span>
             <p className="text-xl font-semibold text-blue-600 pb-4 ">Coins</p>
           </div>
         </section>
         <div className="flex justify-center">
           <p className="text-xl font-semibold text-blue-600 pb-4">
-            {message === "game lost" ? (
-              <p className="text-red-500 text-[24px]">
-                Sorry! you lost the game.
-              </p>
-            ) : (
-              <p className="text-blue-600 text-[24px]">
-                Congrulation you won the game.
-              </p>
-            )}
+            {messageDeliver}
           </p>
         </div>
       </div>
       <div className="flex justify-center items-center">
         <button
           onClick={playGame}
-          className=" bg-amber-500 hover:bg-amber-600 pl-8 pr-8  pt-4 pb-4 rounded-md "
+          className=" bg-amber-500/80 hover:bg-amber-500 pl-8 pr-8  pt-4 pb-4 rounded-md shadow-md text-[18px] font-semibold "
         >
           Play Game
         </button>
