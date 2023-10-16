53d81bf20fa5019d15b1004064897bd6343e773a
saagarbhujel <saagarbhujel22@gmail.com>
2023-10-17 00:19:35 +0545

    navbar & leftaside aded

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Stats from "./components/Stats.tsx";
 import Chat from "./pages/chat/Chat.tsx";
 import Game from "./pages/game/Game.tsx";
 import LeftAside from "./components/LeftAside.tsx";
+import NavBar from "./pages/home/NavBar.tsx";
 
 
 
@@ -27,54 +28,68 @@ const ROLES = {
 function App() {
 
   return (
-   
-    <Routes>
-      
-      <Route path="/" element={<Layout />}>
-        {/* Public Routes */}
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="admin/login" element={<AdminLogin />} />
-        <Route path="unauthorized" element={<Unauthorized/>} />
+    <section>
+   <div className="flex ">
+    <div>
+      <LeftAside />
+    </div>
+    <div>
+      <NavBar />
+    </div>
 
-        {/* Protected Routes */}
-        <Route element={<PresistLogin/>}>
-        {/* <Route> */}
-                <Route element={<RequireAuth allowedRoles={[ROLES.player, ROLES.admin, ROLES.staff]}/>}>
-                  <Route path="/" element={<Home />} />
-                </Route>
+   </div>
+     
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* Public Routes */}
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="admin/login" element={<AdminLogin />} />
+          <Route path="unauthorized" element={<Unauthorized />} />
+          <Route path="/" element={<Home />} />
+          <Route path="dashboard" element={<Dashboard />} />
 
-                 <Route element={<RequireAuth allowedRoles={[ROLES.player]} />}>
-                 {/* <Route> */}
-                  <Route path="profile" element={<Profile />} />
-                  <Route path="stat" element={<Stats />} />
-                  <Route path="chat" element={<Chat />} />
-                  <Route path="game" element={<Game/>} />
+          {/* Protected Routes */}
+          <Route element={<PresistLogin />}>
+            {/* <Route> */}
+            <Route
+              element={
+                <RequireAuth
+                  allowedRoles={[ROLES.player, ROLES.admin, ROLES.staff]}
+                />
+              }
+            >
+              {/* <Route path="/" element={<Home />} /> */}
+            </Route>
 
-                  
-                </Route>
-                {/* <Route> */}
-                <Route element={<RequireAuth allowedRoles={[ROLES.admin]} />}>
-                  <Route path="dashboard" element={<Dashboard />} />
-                </Route>
+            <Route element={<RequireAuth allowedRoles={[ROLES.player]} />}>
+              {/* <Route> */}
+              <Route path="profile" element={<Profile />} />
+              <Route path="stat" element={<Stats />} />
+              <Route path="chat" element={<Chat />} />
+              <Route path="game" element={<Game />} />
+            </Route>
+            {/* <Route> */}
+            <Route element={<RequireAuth allowedRoles={[ROLES.admin]} />}>
+              {/* <Route path="dashboard" element={<Dashboard />} /> */}
+            </Route>
 
-                {/* <Route element={<RequireAuth allowedRoles={[ROLES.staff]} />}>
+            {/* <Route element={<RequireAuth allowedRoles={[ROLES.staff]} />}>
                   <Route path="admin/dashboard" element={<Dashboard />} />
                 </Route> */}
 
-                {/* <Route
+            {/* <Route
                   element={<RequireAuth allowedRoles={[ROLES.admin, ROLES.staff]} />}
                 >
                   <Route path="admin/dashboard" element={<Dashboard />} />
                 </Route> */}
+          </Route>
 
+          {/* Catch for all */}
+          <Route path="*" element={<Missing />} />
         </Route>
-        
-        {/* Catch for all */}
-        <Route path="*" element={<Missing/>} />
-      </Route>
-    </Routes>
-   
+      </Routes>
+    </section>
   );
 }
 
