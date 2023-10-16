2418df92f6fb408d54fafdd9eeb2c3b4bee491a2
saagarbhujel <saagarbhujel22@gmail.com>
2023-10-16 22:47:51 +0545

    greetings added

--- src/pages/home/NavBar.tsx
+++ src/pages/home/NavBar.tsx
@@ -15,11 +15,19 @@ const NavBar = () => {
     }
   }
   return (
-    <nav className=" w-[80vw] h-20 flex items-end">
+    <nav className=" w-[80vw] h-20 flex justify-between items-center mr-10">
       <div className="ml-8 flex flex-row ">
         <p className="font-bold text-black text-[30px] ">
-          {greettings()}, <span className='text-green-500 capitalize'>{details?.name ? details?.name : "Player"}</span>
+          {greettings()},{" "}
+          <span className="text-green-500 capitalize">
+            {details?.name ? details?.name : "Player"}
+          </span>
         </p>
+      </div>
+      <div>
+        <button className=" bg-amber-500/80 hover:bg-amber-500 pl-8 pr-8  pt-4 pb-4 rounded-md shadow-md text-[18px] font-semibold ">
+          Dashboard
+        </button>
       </div>
     </nav>
   );
